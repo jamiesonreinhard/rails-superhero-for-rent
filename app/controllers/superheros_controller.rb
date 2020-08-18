db74fb73b47fdabbfa95ab068f86e59c955c0e32
BENJAMIN CEA <benjaminceavidal@gmail.com>
2020-08-18 12:03:40 -0500

    Completed superhero model, routes and controller

---
+++ app/controllers/superheros_controller.rb
@@ -0,0 +1,52 @@
+class SuperherosController < ApplicationController
+  before_action :set_superhero, only: [:show, :edit, :update, :destroy] 
+#   Create the index action
+def index
+  @superheros = Superhero.all
+end
+# 2.Create the show action
+def show  
+end
+# 3.Create the new action
+def new
+  @superhero = Superhero.new
+end
+# 4.Create the create action
+def create
+  @superhero = Superhero.new(superhero_params)
+  if @superhero.save
+    redirect_to superhero_path(@superhero)
+  else
+    render :new
+  end
+end
+# 5.Create the edit action
+def edit
+end
+# 6.Create the update action
+
+def update
+  @superhero.update(superhero_params)
+  if @superhero.save
+    redirect_to superhero_path(@superhero)
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @superhero.destroy
+  redirect_to superheros_path
+end
+
+private
+
+def set_superhero
+  @superhero = Superhero.find(params[:id])
+end
+
+def superhero_params
+  params.require(:superhero).permit(:name, :city, :superpower, :hourly_price)
+end
+
+end
