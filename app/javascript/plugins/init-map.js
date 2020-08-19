10c66ea36c5870a9fa01d23323ed21adaf918aeb
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-08-19 10:15:27 -0500

    Added address field to superheros table and added javascript init-map file

--- app/javascript/plugins/init-map.js
+++ app/javascript/plugins/init-map.js
@@ -1,28 +1,6 @@
-const addMarker = (coords) => {
-  marker = new google.maps.Marker({
-    position:coords,
-    map: map
-  });
-}
 
-const initGoogleMap = () => {
-  const mapElement = document.getElementById('map');
-
-  const options = {
-    zoom: 8,
-    center: { lat: 19.432608, lng: -99.133209 }
-  }
-  const map = new google.maps.Map(mapElement, options);
+const initMapBox= () => {
   
+};
 
-  marker.addListener('click', () => {
-    infoWindow.open(map, marker);
-  });
-}
-
-// addMarker({ lat: 19.432608, lng: -99.133209 });
-//   infoWindow = new google.maps.InfoWindow({
-//     content: '<h3>Mexico City</h3>'
-//   });
-
-export { initGoogleMap };
+export { initMapBox };
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_141459) do
+ActiveRecord::Schema.define(version: 2020_08_19_151225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_08_19_141459) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_superheros_on_user_id"
   end
 
