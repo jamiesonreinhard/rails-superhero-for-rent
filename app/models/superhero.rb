4aaca978ddb6a164beb849f148ac4ef4375b923b
Valeria Ramey <valeriaramey@gmail.com>
2020-08-19 14:04:01 -0400

    completed JSON parse in the seeds

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,7 +1,7 @@
 class Superhero < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :city, presence: true
   validates :superpower, presence: true
   validates :hourly_price, presence: true
