f94b14a60a22f90b00a7cc1d30654a2a7162b8b7
Valeria Ramey <valeriaramey@gmail.com>
2020-08-19 15:57:21 -0400

    updated address and city in the seeds and changed show html and css

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,5 +1,5 @@
 class Superhero < ApplicationRecord
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,37 @@ require 'open-uri'
 require 'json'
 require 'faker'
 
-# puts "Destroy superheros"
-# Superhero.destroy_all if Rails.env.development?
+puts "Destroy superheros"
+User.destroy_all
+Superhero.destroy_all
+
+
+
+cities = ['Avon', 'Branford', 'Bristol', 'East Meadow', 'East Syracuse', 'Evans Mills', 'Farmingdale', 'Fishskill',
+          'Gardner', 'Hadley', 'Halifax', 'Hanover', 'Hudson', 'Leicester', 'Leominster', 'Lunenburg', 'Norwich',
+          'Old Saybrook', 'Putnam', 'Rocky Hill'
+         ]
+
+addresses = ["255 W Main St, Avon CT 6001",
+  "120 Commercial Parkway, Branford CT 6405",
+  "1400 Farmington Ave, Bristol CT 6010",
+  "2465 Hempstead Turnpike, East Meadow NY 11554",
+  "6438 Basile Rowe, East Syracuse NY 13057",
+  "25737 US Rt 11, Evans Mills NY 13637",
+  "901 Route 110, Farmingdale NY 11735",
+  "2400 Route 9, Fishkill NY 12524",
+   "677 Timpany Blvd, Gardner MA 1440",
+  "337 Russell St, Hadley MA 1035",
+  "295 Plymouth Street, Halifax MA 2338",
+  "1775 Washington St, Hanover MA 2339",
+  "280 Washington Street, Hudson MA 1749",
+  "20 Soojian Dr, Leicester MA 1524",
+  "11 Jungle Road, Leominster MA 1453",
+  "301 Massachusetts Ave, Lunenburg MA 1462",
+   "220 Salem Turnpike, Norwich CT 6360",
+  "655 Boston Post Rd, Old Saybrook CT 6475",
+  "625 School Street, Putnam CT 6260",
+  "80 Town Line Rd, Rocky Hill CT 6067"]
 
 
 puts "Create superheroes"
@@ -12,13 +41,15 @@ puts "Create superheroes"
 
   url = "https://superheroapi.com/api/10101309158912354/#{index+1}"
   superheros_data = JSON.parse(open(url).read)
-  supehero = Superhero.new(name: superheros_data['name'], image_url: superheros_data["image"]["url"],
-    city: Faker::Address.city,
+  superhero = Superhero.new(name: superheros_data['name'],
+    city: cities.sample, address: addresses.sample,
     superpower: Faker::Superhero.power, hourly_price: rand(500...2000), user: User.all.sample)
-    file = URI.open(superheros_data["image"]["url"])
-    supehero.photo.attach(io: file, filename: "#{supeheros_data['name']}.jpeg", content_type: 'image/jpeg')
+    # file = URI.open(superheros_data["image"]["url"])
+    # supehero.photo.attach(io: file, filename: "#{superheros_data['name']}.jpeg", content_type: 'image/jpeg')
     superhero.save
 end
 
+
+# img_url: superheros_data["image"]["url"],
 
 
