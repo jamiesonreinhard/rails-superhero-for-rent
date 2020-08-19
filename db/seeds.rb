53bb639e6b7ed5c727d576ca35c95bfa91e8ea6b
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-08-18 22:58:57 -0500

    seeding code has an error - uninitialized constant Superhero

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+superhero_data = JSON.parse(open("https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json").read)
+
+puts superhero_data[0]["name"]
+
+100.times do
+  random_index = rand(1..700)
+  superindex = superhero_data[random_index]
+  Superhero.create!(name: superindex["name"], city: superindex["work"]["base"] != "-" ? superindex["work"]["base"] : (['New York', 'Los Angeles', 'Houston', 'Chicago', 'Seattle']).sample, superpower: "Butt Kicking", hourly_price: rand(50..1000))
+end
