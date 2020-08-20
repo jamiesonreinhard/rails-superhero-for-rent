acd73d9db65baac1dd1be7c3927dab6441269a9e
Valeria Ramey <valeriaramey@gmail.com>
2020-08-19 21:31:46 -0400

    updated seeds and created users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'json'
 require 'faker'
+require 'pry'
 
 puts "Destroy superheros"
 User.destroy_all
@@ -10,25 +11,31 @@ Superhero.destroy_all
 
 cities = ['New York City', 'Paris', 'Chicago', 'Mexico City', 'Denver', 'Montreal', 'Berlin', 'Moscow',
           'London', 'Los Angeles', 'Toronto', 'Portland', 'Milan', 'Barcelona', 'Sydney']
-         ]
 
 
-puts "Create superheroes"
+valeria = User.create!(first_name: "Valeria", last_name: 'Ramey', email: "valeriaramey@gmail.com", password: "valval")
+iben = User.create!(first_name: "Iben", last_name: 'Lewagon', email: "iben@gmail.com", password: "123456")
+john = User.create!(first_name: "John", last_name: 'Smith', email: "johnsmith@gmail.com", password: "123456")
 
-1..100.times do |index|
+puts "Create superheros"
+
+1..25.times do |index|
 
   url = "https://superheroapi.com/api/10101309158912354/#{index+1}"
   superheros_data = JSON.parse(open(url).read)
   superhero = Superhero.new(name: superheros_data['name'],
-    city: cities.sample, address: addresses.sample, img_url: superheros_data["image"]["url"],
+    city: cities.sample,
     superpower: Faker::Superhero.power, hourly_price: rand(500...2000), user: User.all.sample)
     if (superheros_data["image"]["url"])
       file = URI.open(superheros_data["image"]["url"])
-      superhero.img_url.attach(io: file, filename: "#{superheros_data['name']}.jpeg", content_type: 'image/jpeg')
+
+      superhero.img_url.attach(io: file, filename: "#{superheros_data['name']}.jpg", content_type: 'image/jpg')
     end
+    puts superhero
     superhero.save
+
 end
 
 
 
-
+# img_url: superheros_data["image"]["url"],
