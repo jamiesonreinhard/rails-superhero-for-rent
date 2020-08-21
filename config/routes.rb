266477df96ca71e17961491fb052f2f7a8376511
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-08-21 11:21:28 -0500

    fixed mylistings routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/booking_confirmation', to: 'pages#confirm'
   resources :bookings, only: [:index]
-  resources :users, only: [:index]
+  get '/me', to: "users#index", as: :me
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :superheros do
      resources :bookings
